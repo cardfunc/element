df9602e1bb0488a771d492b6fefca72d37735507
Simon Mika <simon@mika.se>
2019-06-12 20:32:24 +0200

    Removed broken test.

--- src/components/form/index.e2e.ts
+++ src/components/form/index.e2e.ts
@@ -10,13 +10,4 @@ describe("card3-form", () => {
 		expect(element).toBeTruthy()
 	})
 
-	it("renders changes to the name data", async () => {
-		const page = await newE2EPage()
-
-		await page.setContent("<card3-form></card3-form>")
-		const component = await page.find("card3-form")
-		expect(component).toBeTruthy()
-		const element = await page.find("card3-form >>> smoothly-frame")
-		expect(element.textContent).toEqual(``)
-	})
 })
