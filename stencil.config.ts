a1e7f2d10d38f4ed37fc3eeec18a47f359660f24
Simon Mika <simonmika@users.noreply.github.com>
2019-09-03 16:51:44 +0200

    Added ui to path.

--- stencil.config.ts
+++ stencil.config.ts
@@ -10,7 +10,7 @@ export const config: Config = {
 		{
 			type: "www",
 			serviceWorker: null, // disable service workers
-			baseUrl: "/element",
+			baseUrl: "/ui/element",
 		},
 	],
 	globalStyle: "src/style.css",
