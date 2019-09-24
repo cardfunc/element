5e3a89596a4ced674f9328e29201566808483386
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-09-24 15:26:12 +0200

    Minor refactoring

--- src/components/element/index.tsx
+++ src/components/element/index.tsx
@@ -15,19 +15,18 @@ export class Form {
 	@Prop() apiKey: string
 	@Prop({ reflectToAttr: true, mutable: true }) state: "failed" | "succeeded" | "processing" | "created" = "created"
 	@Prop({ mutable: true }) value?: AuthorizationCreatableSafe
-	private themeName?: string
+	@State() theme?: string
 	@Event() changed: EventEmitter<Authorization>
 	private received?: (state: "succeeded" | "failed", authorization: Authorization | Token) => void
 	@State() payload?: Payload
 	componentWillLoad() {
 		new Verifier("public").verify(this.apiKey).then(payload => this.payload = payload)
-		const styleLinks = document.querySelectorAll("link[rel=stylesheet]") as NodeListOf<HTMLLinkElement>
-		styleLinks.forEach(element => {
-			if (element.href.startsWith("https://theme.payfunc.com/")) {
-				const themeLink = element.href.substring(26)
-				this.themeName = themeLink.substring(0, themeLink.indexOf("/"))
-			}
-		})
+		const styleLink = document.querySelector("link[rel=stylesheet][href^='https://theme.payfunc.com/']") as HTMLLinkElement
+		console.log("stylelink is", styleLink)
+		if (styleLink) {
+			const themeLink = styleLink.href.substring(26)
+			this.theme = themeLink.substring(0, themeLink.indexOf("/"))
+		}
 	}
 	@Listen("trigger")
 	async handleTrigger(event: CustomEvent<Trigger>) {
@@ -52,7 +51,7 @@ export class Form {
 	}
 	render() {
 		return [
-			this.payload ? <smoothly-frame url={ this.payload.iss + "/ui/web-app/" + "?theme=" + this.themeName } name="card" ref={ (element: HTMLSmoothlyFrameElement) => this.frame = element }></smoothly-frame> : [],
+			this.payload ? <smoothly-frame url={ this.payload.iss + "/ui/web-app/" + (this.theme ? "?theme=" + this.theme : "") } name="card" ref={ (element: HTMLSmoothlyFrameElement) => this.frame = element }></smoothly-frame> : [],
 			// TODO: Reenable 3D Secure
 			// this.payload && this.value && this.value.verify ?
 			// <smoothly-dialog closable>
