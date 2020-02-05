ac18bc96a95587eb632ab6d1ad5abe984a2815e2
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2020-02-05 10:25:36 +0100

    Initial version of authorization works

--- src/components/element/index.tsx
+++ src/components/element/index.tsx
@@ -16,7 +16,8 @@ export class Form {
 	@Prop({ reflectToAttr: true, mutable: true }) state: "failed" | "succeeded" | "processing" | "created" = "created"
 	@Prop({ mutable: true }) value?: AuthorizationCreatableSafe
 	@State() theme?: string
-	@State() verify?: { pareq: string, url: string, issuer: string, authorization: Authorization.Creatable.Safe, parent: string, key: string }
+	@State() verify?: { pareq: string, url: string, issuer: string }
+	@State() authorization?: AuthorizationCreatableSafe
 	@Event() changed: EventEmitter<Authorization>
 	private received?: (state: "succeeded" | "failed", authorization: Authorization | Token) => void
 	@State() payload?: Payload
@@ -30,15 +31,24 @@ export class Form {
 	}
 	@Listen("trigger")
 	async handleTrigger(event: CustomEvent<Trigger>) {
-		this.changed.emit(this.value = event.detail.value)
-		this.state = event.detail.name == "cardPaymentSuccess" ? "succeeded" : "failed"
-		if (this.received) {
-			this.received(this.state, event.detail.value)
-			this.received = undefined
+		if (event.detail.value.status == 400 && event.detail.value.type == "missing property" && event.detail.value.content && event.detail.value.content.property == "pares") {
+			this.verify = { pareq: event.detail.value.content.pareq, url: event.detail.value.content.url, issuer: this.payload ? this.payload.iss ? this.payload.iss : "" : "" }
+		} else if (event.detail.name == "pares") {
+			this.verify = undefined
+			if (this.authorization)
+				this.frame.send("card", { name: "submit", value: { authorization: { ...this.authorization, pares: event.detail.value }, key: this.apiKey, parent: window.location.origin } })
+		} else {
+			this.changed.emit(this.value = event.detail.value)
+			this.state = event.detail.name == "cardPaymentSuccess" ? "succeeded" : "failed"
+			if (this.received) {
+				this.received(this.state, event.detail.value)
+				this.received = undefined
+			}
 		}
 	}
 	@Method()
 	submit(authorization: AuthorizationCreatableSafe): Promise<Authorization | Token> {
+		this.authorization = authorization
 		return new Promise(callback => {
 			if (this.frame) {
 				this.received = (_, response) => callback(response)
@@ -52,7 +62,7 @@ export class Form {
 			this.payload ? <smoothly-frame url={ this.payload.iss + "/ui/web-app/" + (this.theme ? "?theme=" + this.theme : "") } name="card" ref={ (element: HTMLSmoothlyFrameElement) => this.frame = element }></smoothly-frame> : [],
 			this.verify ?
 			<smoothly-dialog closable>
-				<smoothly-frame url={ `${ this.verify.issuer }/redirect/post?target=${ this.verify.url }&PaReq=${ this.verify.pareq }` } name="parent"></smoothly-frame>
+				<smoothly-frame url={ `${ this.verify.issuer }/redirect/post?target=${ this.verify.url }&PaReq=${ this.verify.pareq }&TermUrl=${ this.verify.issuer }/message?parent=${ window.origin }&MD=MD` } name="parent" style={{ height: "90vh" }}></smoothly-frame>
 			</smoothly-dialog> :
 			[],
 		]
