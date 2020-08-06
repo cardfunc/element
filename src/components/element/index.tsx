6d1cb13329a951b50523539bc8567da98245d2f9
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-08-06 14:03:58 +0200

    Key info (#51)
    * key info simplification
    
    * model bump

--- src/components/element/index.tsx
+++ src/components/element/index.tsx
@@ -1,9 +1,9 @@
 // tslint:disable-next-line:no-implicit-dependencies
 import { Component, Event, EventEmitter, Method, Listen, Prop, State, h } from "@stencil/core"
-import { Payload, Verifier, Token } from "authly"
+import { Token } from "authly"
 import { Error } from "gracely"
 import { Trigger } from "smoothly-model"
-import { AuthorizationCreatableSafe } from "../model"
+import { AuthorizationCreatableSafe, MerchantKeyInfo, MerchantKeyInfoUnpack } from "../model"
 
 @Component({
 	tag: "cardfunc-element",
@@ -24,36 +24,19 @@ export class Form {
 	@Event() changed3d: EventEmitter<AuthorizationCreatableSafe>
 	private received?: (state: "succeeded" | "failed", authorization: AuthorizationCreatableSafe | Error | Token) => void
 	private received3d?: (state3d: "succeeded" | "failed", authorization: AuthorizationCreatableSafe | Error | Token) => void
-	@State() payload?: Payload
-	componentWillLoad() {
-		Verifier.create("public").verify(this.apiKey).then(async payload => {
-			const cardfuncKey = (payload as any)?.option?.card // Old payfunc key
-			this.payload = typeof cardfuncKey == "string" ? (await Verifier.create("public").verify(cardfuncKey)) ?? payload : payload
-		})
+	@State() merchant?: MerchantKeyInfo
+	async componentWillLoad() {
+		this.merchant = await MerchantKeyInfoUnpack(this.apiKey, "public")
 		const styleLink = document.querySelector("link[rel=stylesheet][href^='https://theme.payfunc.com/']") as HTMLLinkElement
 		if (styleLink) {
 			const themeLink = styleLink.href.substring(26)
 			this.theme = themeLink.substring(0, themeLink.indexOf("/"))
 		}
 	}
-	private getMerchantId(): string | undefined {
-		return this.payload
-			? typeof this.payload.card == "object" && (this.payload.card as any)?.id && typeof (this.payload.card as any)?.id == "string"
-			? (this.payload.card as any)?.id
-			: this.payload.sub ?? undefined
-			: undefined
-	}
-	private getCardfuncIssuer(): string {
-		return this.payload
-			? typeof this.payload.card == "object" && (this.payload.card as any)?.url && typeof (this.payload.card as any)?.url == "string"
-			? (this.payload.card as any)?.url
-			: this.payload.iss ?? ""
-			: ""
-	}
 	@Listen("trigger")
 	async handleTrigger(event: CustomEvent<Trigger>) {
 		if (event.detail.value && event.detail.value.status == 400 && event.detail.value.type == "missing property" && event.detail.value.content && event.detail.value.content.property == "pares")
-			this.verify = { pareq: event.detail.value.content.pareq, url: event.detail.value.content.url, issuer: this.getCardfuncIssuer() }
+			this.verify = { pareq: event.detail.value.content.pareq, url: event.detail.value.content.url, issuer: this.merchant?.card.url ?? "" }
 		else if (event.detail.name == "pares" || event.detail.name == "card") {
 			this.verify = undefined
 			if (this.authorization)
@@ -102,11 +85,11 @@ export class Form {
 	}
 	render() {
 		return [
-			this.payload ? <smoothly-frame url={ this.getCardfuncIssuer() + "/ui/web-app/" + (this.theme ? "?theme=" + this.theme : "") } name="card" ref={ (element: HTMLSmoothlyFrameElement) => this.frame = element }></smoothly-frame> : [],
+			this.merchant ? <smoothly-frame url={ this.merchant.card.url + "/ui/web-app/" + (this.theme ? "?theme=" + this.theme : "") } name="card" ref={ (element: HTMLSmoothlyFrameElement) => this.frame = element }></smoothly-frame> : [],
 			this.verify ?
 			<smoothly-dialog closable>
 				<smoothly-frame
-					url={ `${ this.verify.issuer }/redirect/post?target=${ encodeURIComponent(this.verify.url) }&PaReq=${ encodeURIComponent(this.verify.pareq) }&MD=MD&TermUrl=${ encodeURIComponent(`${ this.verify.issuer }/emv3d1/done?merchant=${ this.getMerchantId() ?? "error" }&token=${ this.authorization?.card ?? "error" }&parent=${ window.location.origin }`) }` }
+					url={ `${ this.verify.issuer }/redirect/post?target=${ encodeURIComponent(this.verify.url) }&PaReq=${ encodeURIComponent(this.verify.pareq) }&MD=MD&TermUrl=${ encodeURIComponent(`${ this.verify.issuer }/emv3d1/done?merchant=${ this.merchant?.card.id ?? this.merchant?.sub ?? "error" }&token=${ this.authorization?.card ?? "error" }&parent=${ window.location.origin }`) }` }
 					name="parent"
 					style={{ height: "90vh" }}
 				></smoothly-frame>
--- src/components/model.ts
+++ src/components/model.ts
@@ -3,3 +3,6 @@ import * as model from "@cardfunc/model"
 // tslint:disable-next-line: no-empty-interface
 export interface AuthorizationCreatableSafe extends model.Authorization.Creatable.Safe {
 }
+
+export type MerchantKeyInfo = model.Merchant.Key.KeyInfo
+export const MerchantKeyInfoUnpack = model.Merchant.Key.KeyInfo.unpack
